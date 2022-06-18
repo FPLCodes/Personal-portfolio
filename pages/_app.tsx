264f6bb0c7a8df8968a9118bed05d2cf23ddb3c7
Sahab <sahabferdous1@gmail.com>
2022-06-18 12:07:01 -0400

    Design changes + installed FontAwesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
 const Home: NextPage = () => {
   return (
@@ -10,26 +12,31 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 xl:px-40 text-center">
-        <header className="flex flex-col gap-y-6 h-screen w-full justify-center text-left">
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 xl:px-40 text-left">
+        <header className="flex flex-col gap-y-6 h-screen w-full justify-center text-middle">
           <h1 className="text-5xl xl:text-6xl max-w-xl">Hello 👋 I'm</h1>
           <h1 className="text-5xl xl:text-6xl max-w-xl">Sahab Ul Ferdous</h1>
           <h2 className="text-3xl xl:text-4xl max-w-xl">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </h2>
+          <div>
+            <FontAwesomeIcon
+              icon={faAngleDown}
+              style={{ fontSize: 30, color: "orange" }}
+            />
+          </div>
         </header>
-        <section className="flex flex-col w-full gap-y-40">
-          <div className="flex flex-col flex-1 gap-y-2">
+        <section className="flex flex-col w-full gap-y-40 mt-40">
+          <div className="flex flex-col flex-1 gap-y-2 w-5/6 lg:w-1/2 self-center md:self-start">
             <h1 className="w-max text-2xl">Project 1</h1>
-            <div className="flex gap-x-3 w-max">
+            <div className="flex flex-col md:flex-row gap-x-3 w-full">
               <img
                 src="https://downey.co.nz/wp-content/uploads/2015/09/awards-grey-box-large.jpg"
-                width={"200px"}
-                height={"200px"}
+                className={"self-center md:w-64"}
               />
-              <div>
-                <p className="w-40 mb-4">
+              <div className="w-full flex flex-col justify-between">
+                <p className="mb-4">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
@@ -41,11 +48,15 @@ const Home: NextPage = () => {
             </div>
           </div>
 
-          <div className="flex flex-col flex-1 gap-y-2">
-            <h1 className="w-max text-2xl self-end">Project 2</h1>
-            <div className="flex gap-x-3 w-max self-end">
-              <div>
-                <p className="w-40 mb-4">
+          <div className="flex flex-col flex-1 gap-y-2 w-5/6 lg:w-1/2  self-center md:self-end">
+            <h1 className="w-max text-2xl">Project 2</h1>
+            <div className="flex flex-col md:flex-row gap-x-3 w-full">
+              <img
+                src="https://downey.co.nz/wp-content/uploads/2015/09/awards-grey-box-large.jpg"
+                className={"self-center md:w-64"}
+              />
+              <div className="w-full flex flex-col justify-between">
+                <p className="mb-4">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
@@ -54,17 +65,14 @@ const Home: NextPage = () => {
                   <p>WEBSITE</p>
                 </div>
               </div>
-              <img
-                src="https://downey.co.nz/wp-content/uploads/2015/09/awards-grey-box-large.jpg"
-                width={"200px"}
-                height={"200px"}
-              />
             </div>
           </div>
         </section>
+
+        <section></section>
       </main>
 
-      <footer className="flex h-12 w-full items-center justify-center border-t"></footer>
+      <footer className="flex h-12 w-full items-center justify-center border-t mt-16"></footer>
     </div>
   );
 };
