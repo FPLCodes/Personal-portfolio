4689b038aee32c025c24bb0ab5c8df35c49c7030
Sahab <sahabferdous1@gmail.com>
2022-06-24 11:31:03 -0400

    Added lang to html tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html className="scroll-smooth">
+    <Html lang="en" className="scroll-smooth">
       <Head>
         <link
           rel="stylesheet"
