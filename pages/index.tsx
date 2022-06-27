9f8bad106e9c7a7f3d153be5de8cb6e46904e0bb
Sahab <sahabferdous1@gmail.com>
2022-06-27 18:18:11 -0400

    Added YouTube channel to background

--- pages/index.tsx
+++ pages/index.tsx
@@ -526,6 +526,16 @@ const Home: NextPage = () => {
               <b>When I'm not coding</b> I'm probably gaming, watching anime, or
               doing calisthenics 💪
             </p>
+            <p>
+              Fun fact, I have a gaming{" "}
+              <a
+                className="text-red-500 hover:text-red-300"
+                href="https://www.youtube.com/c/FPLogistic"
+                target="_blank"
+              >
+                YouTube channel!
+              </a>
+            </p>
           </div>
         </section>
 
