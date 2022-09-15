aaffc6e8a562237d5b700ac502c212acda134c12
Sahab <sahabferdous1@gmail.com>
2022-09-15 00:11:00 -0400

    Fixed certificates load animation

--- components/Certificates.tsx
+++ components/Certificates.tsx
@@ -4,15 +4,13 @@ import ffc2 from "../public/fcc2.jpg";
 
 function Certificates() {
   return (
-    <section className="mt-48 w-full md:px-4">
-      <h1
-        data-aos="fade-up"
-        data-aos-duration="700"
-        data-aos-once="true"
-        className="text-4xl font-bold text-orange-500"
-      >
-        Certificates
-      </h1>
+    <section
+      data-aos="fade-up"
+      data-aos-duration="700"
+      data-aos-once="true"
+      className="mt-48 w-full md:px-4"
+    >
+      <h1 className="text-4xl font-bold text-orange-500">Certificates</h1>
       <div className="flex flex-col md:grid grid-cols-2 xl:grid-cols-4 gap-12 pt-16">
         <a
           className="border-4 transition-all drop-shadow hover:drop-shadow-md hover:border-orange-300 duration-500"
