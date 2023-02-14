202ad361dacaadbd258ceecd4dcd84737558c23e
Sahab <sahabferdous1@gmail.com>
2023-02-15 01:41:46 +0800

    Added progress bar at the bottom of the page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,12 @@ import React, { useEffect } from "react";
 import Head from "next/head";
 import Script from "next/script";
 
+import Intro from "../components/Intro";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
+import Certificates from "../components/Certificates";
 import About from "../components/About";
 import Contact from "../components/Contact";
-import Intro from "../components/Intro";
-import Certificates from "../components/Certificates";
 
 const Home: NextPage = () => {
   useEffect(() => {
@@ -17,10 +17,19 @@ const Home: NextPage = () => {
       let sections = document.querySelectorAll("section");
       let sidebarLinks = document.querySelectorAll(".sidebarLink");
 
+      let height = document.documentElement.scrollHeight - screen.height;
+      let progressValue = 0;
+
+      const progressBar = document.querySelector(
+        ".progress-bar"
+      ) as HTMLElement;
+
       window.addEventListener("scroll", function () {
         // Current position of the user's scroll
         let scrollPos =
           document.documentElement.scrollTop || document.body.scrollTop;
+
+        progressValue = scrollPos;
 
         for (let i = 0; i < sections.length; i++) {
           let sectionTop = sections[i].offsetTop - 250; // top position of the current section element - 250 pixels
@@ -32,6 +41,10 @@ const Home: NextPage = () => {
             sidebarLinks[i].classList.remove("text-black");
           }
         }
+
+        // Dynamically set the width of the progress bar
+        progressBar.style.width = `${(progressValue / height) * 100}%`;
+        console.log(scrollPos);
       });
 
       // Automatically smooth scroll to clicked component
@@ -48,7 +61,7 @@ const Home: NextPage = () => {
   }, []);
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center text-zinc-700">
+    <div className="page flex min-h-screen flex-col items-center justify-center text-zinc-700">
       <Head>
         <title>Sahab Ul Ferdous</title>
         <link rel="icon" href="/SF.ico" />
@@ -141,6 +154,10 @@ const Home: NextPage = () => {
             </li>
           </ul>
         </nav>
+
+        <div className="fixed z-50 w-full h-1 bottom-0">
+          <div className="absolute progress-bar w-0 h-full bg-yellow-400"></div>
+        </div>
 
         <Intro />
 
