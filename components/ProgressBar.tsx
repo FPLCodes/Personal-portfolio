56cff22c4c59b150b68ff1403b1059fcfecbea2b
Sahab <sahabferdous1@gmail.com>
2023-07-07 14:32:56 +0800

    Progress bar orange instead of yellow

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -1,7 +1,7 @@
 function progressBar() {
   return (
     <div className="fixed z-50 w-full h-1 bottom-0">
-      <div className="absolute progress-bar w-0 h-full bg-yellow-400"></div>
+      <div className="absolute progress-bar w-0 h-full bg-orange-400"></div>
     </div>
   );
 }
